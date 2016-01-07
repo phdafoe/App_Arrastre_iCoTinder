4b1e77dfafcbf9f2235d409ed1955646e352b2c2
phdafoe <phdafoe@gmail.com>
2016-01-07 16:18:04 +0100

    Initial Commit

---
+++ App_Arrastre_iCoTinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App_Arrastre_iCoTinder
+//
+//  Created by User on 7/1/16.
+//  Copyright © 2016 iCologic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
